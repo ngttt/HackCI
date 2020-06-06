36b56f53ad462c36e0dcbaaa25f6b06d82212778
ngttt <1754050092tien@ou.edu.vn>
2020-06-06 11:32:01 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SignInSignUp from './SignInSignUp';
-
+import Header from './Header';
 function App() {
   return (
     <div className="App">
-        <SignInSignUp/>
+      <Header/>
     </div>
   );
 }
