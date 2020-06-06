6b5f9b029b75720d7360c17a7ce7cf946a740a30
ngttt <1754050092tien@ou.edu.vn>
2020-06-06 09:25:39 +0700

    sign in sign up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignInSignUp from './SignInSignUp';
+
+function App() {
+  return (
+    <div className="App">
+        <SignInSignUp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SignInSignUp.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
+
+import "./index.css";
+
+class SignInSignUp extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="SignInSignUp">
+          <div className="SignInSignUp__Form">
+            <div className="PageSwitcher">
+              <NavLink
+                to="/sign-in"
+                activeClassName="PageSwitcher__Item--Active"
+                className="PageSwitcher__Item"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="PageSwitcher__Item--Active"
+                className="PageSwitcher__Item"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <div className="FormTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign In
+              </NavLink>{" "}
+              or{" "}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm} />
+            <Route path="/sign-in" component={SignInForm} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default SignInSignUp;
